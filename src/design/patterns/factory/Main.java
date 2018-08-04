23fa4b4d725eea808e062ef453260a702d672bd4
Dawid Stodołkiewicz <dawid.stodolkiewicz@empirica.pl>
2018-08-04 21:49:34 +0200

    factory

---
+++ src/design/patterns/factory/Main.java
@@ -0,0 +1,9 @@
+package design.patterns.factory;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Point point = Point.Factory.newPolarPoint(2,3);
+	    
+    }
+}
---
+++ src/design/patterns/factory/Point.java
@@ -0,0 +1,22 @@
+package design.patterns.factory;
+
+
+public class Point {
+    private double x, y;
+
+    private Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static class Factory{
+        public static Point newCartesianPoint(double x, double y){
+            return new Point(x,y);
+        }
+
+        public static Point newPolarPoint(double rho, double theta){
+            return new Point(rho*Math.cos(theta), rho*Math.sin(theta));
+        }
+    }
+
+}
